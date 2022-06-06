f44b29f3ae0da35d7b2fa82ddb1b1c1629f79b9d
Ubuntu <ubuntu@ip-172-26-14-42.us-east-2.compute.internal>
2022-06-06 14:00:02 +0000

    Revised simulation study which lowers ARL and calculates h every time.

---
+++ sim-study/arl-study.R
@@ -0,0 +1,29 @@
+library("here")
+library("tidyverse")
+library("reticulate")
+library("mlmcusum")
+source(here("sim-study", "broken-methods.R"))
+
+# These variables should be passed from sim_study.R, defining them here is
+# only for testing purposes.
+
+# Parameters
+# data_type <- "lin"
+# n_sim     <- 10
+# l         <- 2
+# arl       <- 40
+# n_ic_mod  <- 1000
+# n_ic_h    <- 4000
+# n_oc      <- 4000
+
+# Simulation
+arl_sim <- 1:n_sim |> 
+  map(\(i) {
+    gen_sim_study_brk(data_type = data_type, n_ic_mod = n_ic_mod, n_ic_h = n_ic_h,
+                      n_oc = n_oc, l = l, arl = arl)
+  })
+
+# Save results
+saveRDS(arl_sim, file = here("results", paste0("arl-sim-", data_type, "-", part, ".rds")))
+
+
---
+++ sim-study/broken-methods.R
@@ -0,0 +1,193 @@
+### Imports ###
+library("here")
+library("tidyverse")
+library("reticulate")
+library("mlmcusum")
+
+#path_conda <- "/home/ubuntu/miniconda3/envs/deep-learning-03"
+#path_conda <- "/home/nossimid/miniconda3/envs/deep_learning_v03"
+#use_condaenv(path_conda)
+path_py <- "~/git/mlmcusum/inst/python/gru_functions.py"
+source_python(path_py)
+
+#set.seed(123)
+
+### Get ARL -------------------------------------------------------------------
+gen_sim_study_brk <- function(data_type = "lin", n_ic_mod = 500, n_ic_h = 500, n_oc = 500,
+                              l = 2, arl = 40, ic_arl_first = FALSE) {
+  # Constants
+  id_trn_mod <- 1:n_ic_mod
+  id_trn_h <- (n_ic_mod-l):(n_ic_mod+n_ic_h)
+  id_tst <- (n_ic_mod + n_ic_h - l):(n_ic_mod + n_ic_h + n_oc) # adjust l for varma and htsquare
+  ql <- 1 - 1/arl
+  
+  ### Gen Data ###
+  if(data_type == "lin")      dat <- gen_dat_lin(n_ic_mod + n_ic_h, n_oc)
+  else if(data_type == "nlr") dat <- gen_dat_nrl(n_ic_mod + n_ic_h, n_oc)
+  else if(data_type == "ltm") dat <- gen_dat_ltm(n_ic_mod + n_ic_h, n_oc)
+  
+  
+  # Fit Models
+  fit_gru <- train(dat$none[id_trn_mod, ], method = "gruMCUSUM", lags = l, k = 1.1)
+  fit_mrf <- train(dat$none[id_trn_mod, ], method = "mrfMCUSUM", lags = l, k = 5)
+  fit_vmc <- train(dat$none[id_trn_mod, ], method = "varmaMCUSUM", lags = 1, k = 0.9)
+  fit_vmw <- train(dat$none[id_trn_mod, ], method = "varmaMEWMA", lags = 1, r = 0.3)
+  fit_hts <- train(dat$none[id_trn_mod, ], method = "htsquare")
+  
+  # Predict: To estimate h
+  pred_gru_h <- predict(fit_gru, dat$none[id_trn_h, ])
+  pred_mrf_h <- predict(fit_mrf, dat$none[id_trn_h, ])
+  pred_vmc_h <- predict(fit_vmc, dat$none[id_trn_h, ])
+  pred_vmw_h <- predict(fit_vmw, dat$none[id_trn_h, ])
+  pred_hts_h <- predict(fit_hts, dat$none[id_trn_h, ])
+  
+  # Get h Level
+  h_gru <- quantile(pred_gru_h$pstat, ql)
+  h_mrf <- quantile(pred_mrf_h$pstat, ql)
+  h_vmc <- quantile(pred_vmc_h$pstat, ql)
+  h_vmw <- quantile(pred_vmw_h$pstat, ql)
+  h_hts <- quantile(pred_hts_h$pstat, ql)
+  
+  # Predict: GRU
+  pred_gru_nf <- predict(fit_gru, dat$none[id_tst, ])
+  pred_gru_f1 <- predict(fit_gru, dat$f1[id_tst, ])
+  pred_gru_f2 <- predict(fit_gru, dat$f2[id_tst, ])
+  pred_gru_f3 <- predict(fit_gru, dat$f3[id_tst, ])
+  
+  # Predict: MRF
+  pred_mrf_nf <- predict(fit_mrf, dat$none[id_tst, ])
+  pred_mrf_f1 <- predict(fit_mrf, dat$f1[id_tst, ])
+  pred_mrf_f2 <- predict(fit_mrf, dat$f2[id_tst, ])
+  pred_mrf_f3 <- predict(fit_mrf, dat$f3[id_tst, ])
+  
+  # Predict: VARMA - MCUSUM
+  pred_vmc_nf <- predict(fit_vmc, dat$none[id_tst, ])
+  pred_vmc_f1 <- predict(fit_vmc, dat$f1[id_tst, ])
+  pred_vmc_f2 <- predict(fit_vmc, dat$f2[id_tst, ])
+  pred_vmc_f3 <- predict(fit_vmc, dat$f3[id_tst, ])
+  
+  # Predict: VARMA - MEWMA
+  pred_vmw_nf <- predict(fit_vmw, dat$none[id_tst, ])
+  pred_vmw_f1 <- predict(fit_vmw, dat$f1[id_tst, ])
+  pred_vmw_f2 <- predict(fit_vmw, dat$f2[id_tst, ])
+  pred_vmw_f3 <- predict(fit_vmw, dat$f3[id_tst, ])
+  
+  # Predict: Hotelling's T^2
+  pred_hts_nf <- predict(fit_hts, dat$none[id_tst, ])
+  pred_hts_f1 <- predict(fit_hts, dat$f1[id_tst, ])
+  pred_hts_f2 <- predict(fit_hts, dat$f2[id_tst, ])
+  pred_hts_f3 <- predict(fit_hts, dat$f3[id_tst, ])
+  
+  # Get Run Length: GRU
+  rl_gru_nf <- ifelse(ic_arl_first,
+                      get_first_fault(tail(pred_gru_nf$pstat, n_oc), h_gru),
+                      get_run_length(tail(pred_gru_nf$pstat, n_oc), h_gru))
+  rl_gru_f1 <- get_first_fault(tail(pred_gru_f1$pstat, n_oc), h_gru)
+  rl_gru_f2 <- get_first_fault(tail(pred_gru_f2$pstat, n_oc), h_gru)
+  rl_gru_f3 <- get_first_fault(tail(pred_gru_f3$pstat, n_oc), h_gru)
+  
+  # Get Run Length: MRF
+  rl_mrf_nf <- ifelse(ic_arl_first,
+                      get_first_fault(tail(pred_mrf_nf$pstat, n_oc), h_mrf),
+                      get_run_length( tail(pred_mrf_nf$pstat, n_oc), h_mrf))
+  rl_mrf_f1 <- get_first_fault(tail(pred_mrf_f1$pstat, n_oc), h_mrf)
+  rl_mrf_f2 <- get_first_fault(tail(pred_mrf_f2$pstat, n_oc), h_mrf)
+  rl_mrf_f3 <- get_first_fault(tail(pred_mrf_f3$pstat, n_oc), h_mrf)
+  
+  # Get Run Length: VARMA - MCUSUM
+  rl_vmc_nf <- ifelse(ic_arl_first,
+                      get_first_fault(tail(pred_vmc_nf$pstat, n_oc), h_vmc),
+                      get_run_length( tail(pred_vmc_nf$pstat, n_oc), h_vmc))
+  rl_vmc_f1 <- get_first_fault(tail(pred_vmc_f1$pstat, n_oc), h_vmc)
+  rl_vmc_f2 <- get_first_fault(tail(pred_vmc_f2$pstat, n_oc), h_vmc)
+  rl_vmc_f3 <- get_first_fault(tail(pred_vmc_f3$pstat, n_oc), h_vmc)
+  
+  # Get Run Length: VARMA - MEWMA
+  rl_vmw_nf <- ifelse(ic_arl_first,
+                      get_first_fault(tail(pred_vmw_nf$pstat, n_oc), h_vmw),
+                      get_run_length( tail(pred_vmw_nf$pstat, n_oc), h_vmw))
+  rl_vmw_f1 <- get_first_fault(tail(pred_vmw_f1$pstat, n_oc), h_vmw)
+  rl_vmw_f2 <- get_first_fault(tail(pred_vmw_f2$pstat, n_oc), h_vmw)
+  rl_vmw_f3 <- get_first_fault(tail(pred_vmw_f3$pstat, n_oc), h_vmw)
+  
+  # Get Run Length: Hotelling's T^2
+  rl_hts_nf <- ifelse(ic_arl_first,
+                      get_first_fault(tail(pred_hts_nf$pstat, n_oc), h_hts),
+                      get_run_length( tail(pred_hts_nf$pstat, n_oc), h_hts))
+  rl_hts_f1 <- get_first_fault(tail(pred_hts_f1$pstat, n_oc), h_hts)
+  rl_hts_f2 <- get_first_fault(tail(pred_hts_f2$pstat, n_oc), h_hts)
+  rl_hts_f3 <- get_first_fault(tail(pred_hts_f3$pstat, n_oc), h_hts)
+  
+  # Construct Tibble
+  rl_gru <- tibble(Data = data_type, method = "gruMCUSUM", IC = rl_gru_nf,
+                   F1 = rl_gru_f1, F2 = rl_gru_f2, F3 = rl_gru_f3)
+  rl_mrf <- tibble(Data = data_type, method = "mrfMCUSUM", IC = rl_mrf_nf,
+                   F1 = rl_mrf_f1, F2 = rl_mrf_f2, F3 = rl_mrf_f3)
+  rl_vmc <- tibble(Data = data_type, method = "varMCUSUM", IC = rl_vmc_nf,
+                   F1 = rl_vmc_f1, F2 = rl_vmc_f2, F3 = rl_vmc_f3)
+  rl_vmw <- tibble(Data = data_type, method = "varMEWMA", IC = rl_vmw_nf,
+                   F1 = rl_vmw_f1, F2 = rl_vmw_f2, F3 = rl_vmw_f3)
+  rl_hts <- tibble(Data = data_type, method = "Hotelling-T2", IC = rl_hts_nf,
+                   F1 = rl_hts_f1, F2 = rl_hts_f2, F3 = rl_hts_f3)
+  # Save Pstat
+  pstat_gru <- tibble(Data = data_type, method = "gruMCUSUM",
+                      IC = c(fit_gru$pstat, pred_gru_nf$pstat),
+                      F1 = c(fit_gru$pstat, pred_gru_f1$pstat),
+                      F2 = c(fit_gru$pstat, pred_gru_f2$pstat),
+                      F3 = c(fit_gru$pstat, pred_gru_f3$pstat))
+  
+  pstat_mrf <- tibble(Data = data_type, method = "mrfMCUSUM",
+                      IC = c(fit_mrf$pstat, pred_mrf_nf$pstat),
+                      F1 = c(fit_mrf$pstat, pred_mrf_f1$pstat),
+                      F2 = c(fit_mrf$pstat, pred_mrf_f2$pstat),
+                      F3 = c(fit_mrf$pstat, pred_mrf_f3$pstat))
+  
+  pstat_vmc <- tibble(Data = data_type, method = "varMCUSUM",
+                      IC = c(fit_vmc$pstat, pred_vmc_nf$pstat),
+                      F1 = c(fit_vmc$pstat, pred_vmc_f1$pstat),
+                      F2 = c(fit_vmc$pstat, pred_vmc_f2$pstat),
+                      F3 = c(fit_vmc$pstat, pred_vmc_f3$pstat))
+  
+  pstat_vmw <- tibble(Data = data_type, method = "varMEWMA",
+                      IC = c(fit_vmw$pstat, pred_vmw_nf$pstat),
+                      F1 = c(fit_vmw$pstat, pred_vmw_f1$pstat),
+                      F2 = c(fit_vmw$pstat, pred_vmw_f2$pstat),
+                      F3 = c(fit_vmw$pstat, pred_vmw_f3$pstat))
+  
+  pstat_hts <- tibble(Data = data_type, method = "Hotelling-T2",
+                      IC = c(fit_hts$pstat, pred_hts_nf$pstat),
+                      F1 = c(fit_hts$pstat, pred_hts_f1$pstat),
+                      F2 = c(fit_hts$pstat, pred_hts_f2$pstat),
+                      F3 = c(fit_hts$pstat, pred_hts_f3$pstat))
+  
+  ### Return
+  list(dat_pstat = bind_rows(pstat_gru,
+                             pstat_mrf,
+                             pstat_vmc[-c(1:l), ],
+                             pstat_vmw[-c(1:l), ],
+                             pstat_hts[-c(1:l), ]) |> 
+         mutate(Data = as_factor(Data), method = as_factor(method)),
+       dat_rl = bind_rows(rl_gru,
+                          rl_mrf,
+                          rl_vmc,
+                          rl_vmw,
+                          rl_hts) |> 
+         mutate(Data = as_factor(Data), method = as_factor(method)),
+       dat_h = tibble(h = c("gruMCUSUM", "mrfMCUSUM", "varMCUSUM",
+                            "varMEWMA", "Hotelling-T2"),
+                      value = c(h_gru, h_mrf, h_vmc, h_vmw, h_hts)))
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
