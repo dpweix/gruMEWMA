aa0dfd0167e5f62b4f753eb51b35d08b653de17c
dpweix <dpweix@gmail.com>
2023-08-14 11:38:14 -0400

    Adds code for run length histogram generation

--- 7-visualize-study.R
+++ 7-visualize-study.R
@@ -4,11 +4,14 @@ theme_set(theme_bw())
 theme_update(plot.title = element_text(hjust = 0.5, size = 15))
 library("reticulate")
 library("mlmewma")
+             
 
 # load python
 path_py <- "~/git/mlmewma/inst/python/gru_functions.py"
 source_python(path_py)
 
+
+### Data Generation Figures --------------------------------------------------
 # Where to save figures
 fig_path <- here("figures/")
 phi <- 0.8
@@ -23,7 +26,7 @@ dat_ltm <- gen_dat_ltm(n_ic = 1000, n_oc = 1000, phi = phi)
 # Generate all labels for plots
 titles <- 
   expand.grid(c("Linear Stationary:"    , "Linear Non-stationary:",
-                "Non-linear Stationary:", "Non-Linear Non-stationary:"),
+                "Non-linear Stationary:", "Non-linear Non-stationary:"),
               c("No Fault", "Fault 1", "Fault 2", "Fault 3")) |> 
   arrange(Var1) |>
   pmap_chr(paste)
@@ -96,3 +99,56 @@ plots$`Non-linear Non-stationary: Fault 3`
                   ".png"),
            plots[[n]], units = "cm", width = 12, height = 8)
   })
+
+### ARL Results Figures --------------------------------------------------
+theme_set(theme_bw())
+theme_update(plot.title = element_text(hjust = 0.5, size = 15),
+             plot.subtitle = element_text(hjust = 0.5, size = 10),
+             strip.text = element_text(size = 13),
+             strip.placement = "outside",
+             
+             strip.background = element_blank())
+# Parameters for study
+data_type <- c("lin", "ltl", "nlr", "ltm")
+phi       <- c(0, 0.4, 0.8)
+data_label <- 
+  c("Linear Stationary:", "Linear Non-stationary:", 
+    "Non-linear Stationary:", "Non-linear Non-stationary:") |> 
+  set_names(c("lin", "ltl", "nlr", "ltm"))
+
+expand_grid(data_type, phi) |> 
+  pmap(\(data_type, phi) {
+    # Plot Run Lengths
+    df_rl <- 
+      1:(n_sim) |> 
+      purrr::map_dfr(\(i) {
+        
+        arl_sim <- readRDS(here("results", paste0("arl-sim-", phi,"-", data_type, "-", i, ".rds")))
+        arl_sim$rl
+        
+      })
+    
+    rl_histogram <- 
+      df_rl |> 
+      group_by(method, phi, data) |> 
+      mutate(nf_median = median(nf),
+             nf_mean = mean(nf)) |> 
+      ungroup() |> 
+      ggplot(aes(nf)) +
+      geom_histogram(bins = 80) +
+      geom_vline(aes(xintercept = nf_median), color = "red") +
+      geom_vline(aes(xintercept = nf_mean), color = "blue") +
+      scale_x_continuous(limits = c(0, 800)) + 
+      #scale_y_continuous(limits = c(0, 200)) +
+      labs(title = paste0(data_label[[data_type]], " phi = ", phi),
+           x = "Run Length", y = "Count") +
+      facet_wrap(vars(method), ncol = 4, scales = "free_x")
+    
+    ggsave(here("figures", paste0("rl-histogram-", phi,"-", data_type, ".png")),
+           plot = rl_histogram,
+           width = 5000,
+           height = 3000,
+           units = "px")
+  })
+
+
